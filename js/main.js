98a891cd0ebf455ac4eee3bc380fe6479b8a3776
h2vt <hidrogenoverdeturquesa@gmail.com>
2026-08-16 11:56:26 -0500

    Remove Laboratory button border

--- js/main.js
+++ js/main.js
@@ -820,7 +820,7 @@
         if (LAB_ENABLED && !document.querySelector('script[data-hvt-lab]')) {
             const labStyle = document.createElement('link');
             labStyle.rel = 'stylesheet';
-            labStyle.href = '/css/laboratorio.css?v=20260816f';
+            labStyle.href = '/css/laboratorio.css?v=20260816g';
             document.head.appendChild(labStyle);
 
             const labScript = document.createElement('script');
